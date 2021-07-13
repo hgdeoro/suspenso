7167d68d2e8f08fc97d18914c6c90ddb0cb059ff
Horacio G. de Oro <hgdeoro@gmail.com>
2021-07-13 16:01:49 +0300

    Rename module and refactor variable names

---
+++ suspensopy/dump_messages.py
@@ -0,0 +1,32 @@
+import pathlib
+import struct
+import sys
+
+MAX_SIZE = 256
+
+
+def main():
+    input_file = pathlib.Path(sys.argv[1])
+    assert input_file.exists() and input_file.is_file()
+    iter_num = 1
+    with input_file.open(mode='rb') as file_obj:
+        while True:
+            num_messages_bytes = file_obj.read(4)
+            if not len(num_messages_bytes):
+                break
+
+            num_messages = struct.unpack('<I', num_messages_bytes)[0]
+
+            assert 1 <= num_messages <= MAX_SIZE, f"Invalid num_messages: {num_messages}"
+
+            remaining_size = 8 + (num_messages - 1) * 12
+            message = file_obj.read(remaining_size)
+            assert len(message) == remaining_size, \
+                f"read() returned {len(message)} bytes, expected {remaining_size} bytes"
+
+            print(f"num_messages={num_messages}")
+            iter_num += 1
+
+
+if __name__ == '__main__':
+    main()
